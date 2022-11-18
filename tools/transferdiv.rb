11f341a65525357d3c4aee435325746174092476
Saul Kravitz <skravitz@mitre.org>
2022-11-18 13:10:20 -0500

    added some documentation

--- tools/transferdiv.rb
+++ tools/transferdiv.rb
@@ -1,4 +1,8 @@
+# run ._genonce.sh
+# cd ../CapStatement
+# python3 R4CapStatement_NarrativeMaker.py CapabilityStatement-CodexRTServerCapabilityStatement.json ../Codex-Radiation-Therapy/output
 # ruby transferdiv.rb <json file with narrative in text.div> <fsh file to be updated> <new fsh file>
+# ruby tools/transferdiv.rb ../CapStatement/Narrative-CapabilityStatement-CodexRTServerCapabilityStatement.json input/fsh/DEF_CapStmt.fsh DEF_CapStmt.fsh
 
 require "json"
 require "byebug"
